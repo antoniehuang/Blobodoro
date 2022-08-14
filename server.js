314b2d2d85ec46e5a10199b6cfaac6652780a2a9
antoniehuang <antonieyuchi@gmail.com>
2022-08-14 12:32:22 +0100

    Setup server.js to listen and response with HTML template

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+const PORT = 8000
+require('dotenv').config()
+
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'studyhistory'
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connect to ${dbName} Database`)
+        db = client.db(dbName)
+    })
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/', (request, response) => {
+    response.render('index.ejs')
+})
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(process.env.PORT)
+    console.log(`Server running on port ${process.env.PORT} or ${PORT}`)
+})
